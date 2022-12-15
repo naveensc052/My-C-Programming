08a313c5aa080a2ecf38582d3f28c7e8a9c505e4
Naveen <106677907+naveensc052@users.noreply.github.com>
2022-12-15 22:27:10 +0530

    Update storing_odd _and_even_integers_in_separate_variable.c

--- Dynamic Memory Allocation/storing_odd _and_even_integers_in_separate_variable.c
+++ Dynamic Memory Allocation/storing_odd _and_even_integers_in_separate_variable.c
@@ -2,15 +2,17 @@
 #include<stdlib.h>
 int main(){
     int n,*ptr,*ptr2,*ptr3,k=0,l=0;
-    scanf("%d",&n);
+    scanf("%d",&n);  //Number of intergers to be seperated
     ptr = (int*) malloc(n*sizeof(int));
     ptr2 = (int*) malloc(n*sizeof(int));
     ptr3 = (int*) malloc(n*sizeof(int));
     printf("Enter n elements");
+    //entering n elements
     for(int i=0;i<n;i++)
     {
         scanf("%d",ptr+i);
     }
+    //storing all odd integers in ptr2
     for(int i=0;i<n;i++)
     {
         if(*(ptr+i)%2==1)
@@ -19,6 +21,7 @@ int main(){
             k++;
         }
     }
+    //storing all even integers in ptr3
     for(int i=0;i<n;i++)
     {
         if(*(ptr+i)%2==0)
@@ -28,11 +31,13 @@ int main(){
         }
     }
     printf("odd numbers\n");
+    //Printing all odd numbers
     for(int i=0;i<k;i++)
     {
         printf("%d\n",*(ptr2+i));
     }
     printf("even numbers\n");
+    //printing all even integers
     for(int i=0;i<l;i++)
     {
         printf("%d\n",*(ptr3+i));
